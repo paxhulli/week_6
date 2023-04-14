5245495645daa2cf5e7bf7f5d39f759378c4b2c5
paxhulli <paxhulli@hotmail.com>
2023-04-14 11:53:37 +0000

    myfirstcommit

--- simpleionicapp/src/app/home/home.page.ts
+++ simpleionicapp/src/app/home/home.page.ts
@@ -10,11 +10,16 @@ export class HomePage {
   constructor() {}
     myVariable: string ="The force is with me!";
     myVariable2: string ="Hello from CCT";
+    myVariable3: string ="Third message";
     
     updateMyValue(){
       this.myVariable = "Now the force is even stronger!!";
     }
     updateMyValue2(){
       this.myVariable2 = "Hello from Ionic Mobile Development";
+    }
+    updateMyValue3(){
+      this.myVariable3 = "Hello from Third button";
+      console.log("Hello from Third button");
     }
 }
